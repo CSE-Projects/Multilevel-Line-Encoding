ab82436f06bd84895f7817806b453c5cb68c4627
dibyadarshan <dibyadarshan.hota@gmail.com>
2018-02-19 05:57:48 +0530

    2B1Q initialization complete

---
+++ javascripts/encoding_2b1q.js
@@ -0,0 +1,20 @@
+var input = ["00","11","01","10","01"] 
+var x1=[];
+var y1=[];
+var i,j,k;
+
+
+// Replace 5 by no of input
+x1.push(0);
+
+
+for(i=1;i<5;++i){
+	x1.push(i);
+	x1.push(i);
+}
+
+
+x1.push(5);
+
+
+var previouslevel=1; // Initial positive level
