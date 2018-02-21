d10e796af25a6bcd773aa5c834f95de2b996e08d
dibyadarshan <dibyadarshan.hota@gmail.com>
2018-02-21 07:39:30 +0530

    8B6T initialized

---
+++ javascripts/encoding_8b6t.js
@@ -0,0 +1,18 @@
+var p = ["11","53","50"] // p is for plot
+
+var x1=[];
+
+var y1=[];
+
+var i,j,k;
+
+// Replace 3 by no of input
+x1.push(0);
+
+for(i=1;i<(6*3);++i){
+	x1.push(i);x1.push(i);
+}
+
+x1.push(18);
+
+var last=0; // Initial last is 0
